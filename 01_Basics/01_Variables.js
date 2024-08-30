75f737c56753701aef07966bda173ea774925fd9
shubhambagal75 <106662262+shubhambagal75@users.noreply.github.com>
2024-08-30 16:35:57 +0000

    Types of Variables in Js

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 123455;
+console.log('accountId ==>', accountId);
+let accountEmail = 'shubham@salesforce.com';
+var accountPassword = '2345';
+accountCity = 'Pune';
+//accountId = 55555;
+accountEmail = 'Shubham@google.com';
+accountPassword = '8888';
+accountCity = 'Mumbai';
+console.log(typeof accountCity);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+let myName;
+console.log(myName);
