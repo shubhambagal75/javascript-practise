a7aaa394074536536980eecd016de5eb9aa536f7
shubhambagal75 <106662262+shubhambagal75@users.noreply.github.com>
2024-08-30 17:12:43 +0000

    Data types

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3);  We are using nodeJs not browser
+
+let n = 10;
+console.log(typeof n);
+let m;
+let o = null;
+console.log(typeof o);
+let p = 0;
+let q = undefined;
+console.log(typeof q);
+console.log(m);
+console.log(n);
+console.log(o);
+console.log(p);
+console.log(q);
+console.log(o == p);
+console.log(m == p);
+console.log(n = p);
+
+
+
+
+
