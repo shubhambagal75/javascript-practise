495fdcf406239eb0811b5201b0a912051ab2d6e9
shubhambagal75 <106662262+shubhambagal75@users.noreply.github.com>
2024-08-31 12:06:56 +0000

    data types

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -2,6 +2,7 @@
 
 // alert(3 + 3);  We are using nodeJs not browser
 
+/*
 let n = 10;
 console.log(typeof n);
 let m;
@@ -18,6 +19,25 @@ console.log(q);
 console.log(o == p);
 console.log(m == p);
 console.log(n = p);
+
+
+let myName = 'Shaktiman';
+console.log(myName);
+let myName2 = myName;
+myName2 = 'Gangadhar';
+console.log(myName2);
+
+let myName = 'Shaktiman';
+let myArray = [4, 5, 6, 7];
+
+
+let details = [1,2,3,4,5]
+let details2 = details;
+// details2.C = 'Mumbai'
+details2[1] = 8;
+console.log(details);
+console.log(details2);
+*/
 
 
 
